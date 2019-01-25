eccadb1c2069bb1a46040b6b5dfeeb5f7760181a
akump <kumpaw@mail.uc.edu>
2019-01-25 16:08:52 -0500

    email inital changes

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,35 @@
 var express = require( 'express' );
+var nodemailer = require( 'nodemailer' );
+
+var transporter = nodemailer.createTransport( {
+    service: 'gmail',
+    auth: {
+        user: 'kumpandrew@gmail.com',
+        pass: ''
+    }
+} );
+
+var mailOptions = {
+    from: 'kumpandrew@gmail.com',
+    to: 'kumpaw@mail.uc.edu',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
 
 var router = express.Router();
 
 router.get( '/about', function( req, res, next ) {
     res.render( "about" );
+} );
+
+router.post( '/sendMail', function( req, res, next ) {
+    transporter.sendMail( mailOptions, function( error, info ) {
+        if ( error ) {
+            console.log( error );
+        } else {
+            console.log( 'Email sent: ' + info.response );
+        }
+    } );
 } );
 
 module.exports = router;
