e872793a0c6bb063c2f41756847ab63d4b44f538
akump <kumpaw@mail.uc.edu>
2019-01-30 17:02:31 -0500

    shopping cart notifcation now updates when editing cart

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -159,12 +159,12 @@ $( document ).ready( function() {
 } );
 
 $( document ).ready( function() {
-    $( '.quantity-left-minus' ).on('click', function( ) {
-        var cartItemInput = $(this).closest('.input-group').find("#quantity");
+    $( '.quantity-left-minus' ).on( 'click', function() {
+        var cartItemInput = $( this ).closest( '.input-group' ).find( "#quantity" );
         var cartItemInputValue = cartItemInput.val();
-        cartItemInput.val(parseInt(cartItemInputValue) - 1);
+        cartItemInput.val( parseInt( cartItemInputValue ) - 1 );
         cartItemInput.change();
-        
+
         // if(cartItemInputValue - 1 > 1){ 
         //     cartItemInput.val(parseInt(cartItemInputValue) - 1);
         //     cartItemInput.change();
@@ -177,30 +177,30 @@ $( document ).ready( function() {
 } );
 
 $( document ).ready( function() {
-    $( '.quantity-right-plus' ).on('click', function( ) {
-        var cartItemInput = $(this).closest('.input-group').find("#quantity");
+    $( '.quantity-right-plus' ).on( 'click', function() {
+        var cartItemInput = $( this ).closest( '.input-group' ).find( "#quantity" );
         var cartItemInputValue = cartItemInput.val();
-        cartItemInput.val(parseInt(cartItemInputValue) + 1);
+        cartItemInput.val( parseInt( cartItemInputValue ) + 1 );
         cartItemInput.change();
     } );
 } );
 
 $( document ).ready( function() {
-    $( '#quantity' ).change( function( ) {
-        var itemName = $(this).closest("tr").find('th').html();
+    $( '#quantity' ).change( function() {
+        var itemName = $( this ).closest( "tr" ).find( 'th' ).html();
         $.ajax( {
-            method: 'POST',
-            url: '/updateCartItemQuantities',
-            data:{
-                itemName: itemName,
-                quantity: $(this).val()
-            }
-        } )
-        .done( function( msg ) {
-            console.log( "Cart updated" )
-        } )
-        .fail( function( msg ) {
-            console.log( "Cart update failed" )
-        } );
+                method: 'POST',
+                url: '/updateCartItemQuantities',
+                data: {
+                    itemName: itemName,
+                    quantity: $( this ).val()
+                }
+            } )
+            .done( function( msg ) {
+                updateShoppingCartTotal();
+            } )
+            .fail( function( msg ) {
+                console.log( "Cart update failed" )
+            } );
     } );
 } );
