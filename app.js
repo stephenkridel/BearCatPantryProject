1bf33f822b509bd8fa94aab4de0bd0a59a1adfcd
akump <kumpaw@mail.uc.edu>
2019-02-01 19:25:35 -0500

    remove db vital info

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use( bodyParser.urlencoded( {
 } ) );
 
 mongoose.Promise = global.Promise;
-mongoose.connect( "mongodb+srv://bearcatAdmin:ferrari275@bearcatpantry-ahnj6.mongodb.net/test?retryWrites=true", {
+mongoose.connect( `mongodb+srv://bearcatAdmin:${process.env.DB_PW}@bearcatpantry-ahnj6.mongodb.net/test?retryWrites=true`, {
     useNewUrlParser: true
 } ).then(
     () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
