23744d2b0c988167858bf1957da81e5421e62829
akump <kumpaw@mail.uc.edu>
2019-02-10 12:38:48 -0500

    barcodes are now in an array

--- models/itemModel.js
+++ models/itemModel.js
@@ -2,7 +2,7 @@ var mongoose = require( 'mongoose' );
 
 var itemSchema = new mongoose.Schema( {
     itemName: String,
-    barcode: Number,
+    barcodes: [ Number ],
     quantity: Number,
     weight: Number,
     img: {
